ff360c05e112b7e9810b147ee37e30fe4f7ff33c
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-04 13:06:24 -0400

    Initial Commit

---
+++ AutoLayout/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  AutoLayout
+//
+//  Created by Jason Cox on 6/4/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
